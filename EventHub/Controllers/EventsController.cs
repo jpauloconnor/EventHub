98d05a7ffd6e32d7663f051072b7d22649764571
Paul O'Connor <jamespauloconnor@gmail.com>
2016-08-13 11:28:45 -0400

    Events controller. EventsViewModel. Events View file with Bootstrap buttons and fields

---
+++ EventHub/Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿using EventHub.Models;
+using EventHub.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventHub.Controllers
+{
+    public class EventsController : Controller
+    {
+        private ApplicationDbContext _context;
+        
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        // GET: Events
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel
+            {
+                Topics = _context.Topics.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ EventHub/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using EventHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventHub.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Topic { get; set; }
+
+        public IEnumerable<Topic> Topics { get; set; }
+    }
+}
